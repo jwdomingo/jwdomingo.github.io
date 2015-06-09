3f2de28c62772aef96239d39a23ad55bbd5095a0
John Domingo <johnwill.domingo@gmail.com>
2015-06-08 21:37:37 -0700

    Tweaked header parallax for smoother positioning

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,6 @@ $(window).scroll(function() {
   var wScroll = $(this).scrollTop();
 
   $('.fg-img-1').css({
-    'background-position' : (50 + wScroll / 20) + '% ' + (100 - wScroll / 4) + '%'
+    'background-position' : (50 + wScroll / 25) + '% ' + (100 - wScroll / 5.5) + '%'
   });
 });
