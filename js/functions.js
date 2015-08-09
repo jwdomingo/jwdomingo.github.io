e8d4891c740c0b56cdfe268ca8a96c4ebf4641fe
John Domingo <johnwill.domingo@gmail.com>
2015-08-09 16:23:05 -0700

    Removed jQuery scroll blur

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,27 @@
-$(window).scroll(function() {
-  //e.preventDefault();
-
-  var wScroll = $(this).scrollTop();
-      wHeight = $(this).height(),
-      sectionTop = wHeight + $('section h1').position().top + 20;
-
-  console.log(wScroll);
-
-  $('.fg-img-1').css({
-    'background-position' : (50 + wScroll / 25) + '% ' + (100 - wScroll / 5.5) + '%'
-  });
-
-  if (wScroll > sectionTop) {
-    $('header').css({'filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
-    $('header').css({'-webkit-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
-    $('header').css({'-moz-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
-    $('header').css({'-ms-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
-    $('header').css({'-o-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
-  } else {
-    $('header').css({'filter': 'blur(0px)'});
-    $('header').css({'-webkit-filter': 'blur(0px)'});
-    $('header').css({'-moz-filter': 'blur(0px)'});
-    $('header').css({'-ms-filter': 'blur(0px)'});
-    $('header').css({'-o-filter': 'blur(0px)'});
-  }
-});
+// $(window).scroll(function() {
+//   //e.preventDefault();
+//
+//   var wScroll = $(this).scrollTop();
+//       wHeight = $(this).height(),
+//       sectionTop = wHeight + $('section h1').position().top + 20;
+//
+//   console.log(wScroll);
+//
+//   $('.fg-img-1').css({
+//     'background-position' : (50 + wScroll / 25) + '% ' + (100 - wScroll / 5.5) + '%'
+//   });
+//
+//   if (wScroll > sectionTop) {
+//     $('header').css({'filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
+//     $('header').css({'-webkit-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
+//     $('header').css({'-moz-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
+//     $('header').css({'-ms-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
+//     $('header').css({'-o-filter': 'blur(' + Math.round(wScroll / 250) + 'px)'});
+//   } else {
+//     $('header').css({'filter': 'blur(0px)'});
+//     $('header').css({'-webkit-filter': 'blur(0px)'});
+//     $('header').css({'-moz-filter': 'blur(0px)'});
+//     $('header').css({'-ms-filter': 'blur(0px)'});
+//     $('header').css({'-o-filter': 'blur(0px)'});
+//   }
+// });
