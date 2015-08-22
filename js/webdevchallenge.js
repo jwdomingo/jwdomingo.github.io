8994e72f098087ae0964d6816c8ae479d956be51
John Domingo <johnwill.domingo@gmail.com>
2015-08-22 14:59:54 -0700

    Refactored webdevchallenge markup and jQuery

--- js/webdevchallenge.js
+++ js/webdevchallenge.js
@@ -13,16 +13,12 @@ $.ajax({
       var labels = [];
       var sources = [];
 
-      // $('#webdevchallenge .container ul').append(
-      //   $('<li />', {
-      //     text: title
-      //   })
-      // );
-
-      $('#webdevchallenge .challenge' + (i + 1)).append(
-          "<a href='" + link + "'><h3>" + title + '</h3></a>',
-          '<h6> Challenge ' + challenge + ' - Project ' + project + " | <a href='http://johndomingo.com/webdevchallenge'>" + theme + '</a>',
-          '<p>' + description + '</p>'
+      $('#webdevchallenge .challenge' + challenge).append(
+          '<div class=\'project\'>' +
+          '<a href=\'' + link + '\'><h3>' + title + '</h3></a>' +
+          '<h6>Challenge ' + challenge + ' - Project ' + project + ' | <a href=\'http://johndomingo.com/webdevchallenge\'>' + theme + '</a></h6>' +
+          '<p>' + description + '</p>' +
+          '</div>'
         ); //append link element
       }); // each channel item
   },
@@ -30,20 +26,3 @@ $.ajax({
     console.log("Error in xml feed from webdevchallenge");
   }
 });
-
-
-// $(function(){
-//     url = 'http://www.thetutlage.com/rss.xml';
-//     $.ajax({
-//       type: "GET",
-//       url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
-//       dataType: 'json',
-//       error: function(){
-//           alert('Unable to load feed, Incorrect path or invalid feed');
-//       },
-//       success: function(xml){
-//           values = xml.responseData.feed.entries;
-//           console.log(values);
-//       }
-//     });
-// });
